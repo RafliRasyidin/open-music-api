e4c68961b722afe60ccbd34170acb67ea726c6a9
raflirasyidin <raflirasyidin20@gmail.com>
2025-03-06 14:56:18 +0700

    prepare response api handler

--- src/api/handler.js
+++ src/api/handler.js
@@ -3,39 +3,89 @@ class MusicHandler {
         this._service = service
     }
 
-    postAlbumHandler() {
-
+    postAlbumHandler(request, h) {
+        const { name, year } = request.payload;
+        const response = h.response({
+            status: 'success',
+            data: {
+                album_id,
+            },
+        });
+        response.code(201);
+        return response;
     }
 
-    getAlbumByIdHandler() {
-
+    getAlbumByIdHandler(request) {
+        const { id } = request.params;
+        return {
+            status: 'success',
+            data: {
+                album,
+            },
+        };
     }
 
-    putAlbumByIdHandler() {
-
+    putAlbumByIdHandler(request) {
+        const { id } = request.params;
+        return {
+            status: 'success',
+            message: 'Berhasil edit album',
+        };
     }
 
-    deleteAlbumByIdHandler() {
-
+    deleteAlbumByIdHandler(request) {
+        const { id } = request.params;
+        return {
+            status: 'success',
+            message: 'Berhasil menghapus album',
+        }
     }
 
-    postSongsHandler() {
-
+    postSongsHandler(request, h) {
+        const { title, year, genre, performer, duration, albumId } = request.payload;
+        const response = h.response({
+            status: 'success',
+            data: {
+                songId,
+            }
+        });
+        response.code(201);
+        return response;
     }
 
     getSongsHandler() {
-
+        return {
+            status: 'success',
+            data: {
+                songs,
+            }
+        };
     }
 
-    getSongByIdHandler() {
-
+    getSongByIdHandler(request) {
+        const { id } = request.params;
+        return {
+            status: 'success',
+            data: {
+                song,
+            },
+        };
     }
 
-    putSongByIdHandler() {
-
+    putSongByIdHandler(request) {
+        const { id } = request.params;
+        const { title, year, genre, performer, duration, albumId } = request.payload;
+        return {
+            status: 'success',
+            message: 'Berhasil edit lagu'
+        }
     }
 
     deleteSongByIdHandler() {
-        
+        const { id } = request.params;
+        return {
+            status: 'success',
+            message: 'Berhasil hapus lagu',
+        };
     }
 }
