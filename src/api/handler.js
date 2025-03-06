0eb47568d17178bf09f86032ec10f880c45c1a0d
raflirasyidin <raflirasyidin20@gmail.com>
2025-03-06 14:33:48 +0700

    setup handler functions

---
+++ src/api/handler.js
@@ -0,0 +1,41 @@
+class MusicHandler {
+    constructor(service) {
+        this._service = service
+    }
+
+    postAlbumHandler() {
+
+    }
+
+    getAlbumByIdHandler() {
+
+    }
+
+    putAlbumByIdHandler() {
+
+    }
+
+    deleteAlbumByIdHandler() {
+
+    }
+
+    postSongsHandler() {
+
+    }
+
+    getSongsHandler() {
+
+    }
+
+    getSongByIdHandler() {
+
+    }
+
+    putSongByIdHandler() {
+
+    }
+
+    deleteSongByIdHandler() {
+        
+    }
+}
