bb587a9208108a70d2f17302ea6943616b3cd519
raflirasyidin <raflirasyidin20@gmail.com>
2025-03-07 13:31:08 +0700

    add utils for mapping DTO

---
+++ src/utils/index.js
@@ -0,0 +1,23 @@
+const mapSongDBToModel = ({
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    albumId,
+    createdAt,
+    updatedAt
+}) => {
+    id,
+    title,
+    year,
+    performer,
+    genre,
+    duration,
+    albumId,
+    createdAt,
+    updatedAt
+};
+
+module.exports = { mapSongDBToModel };
